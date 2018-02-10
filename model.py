e3abc5e56ceac549d85df3ca31e1f0e940debc53
dzhao <dzhao@rhr-rp-007.righthandrobotics.com>
2018-02-10 05:54:15 -0500

    I think I have the interface functions working

--- model.py
+++ model.py
@@ -7,20 +7,17 @@ from keras.losses import mean_squared_error, categorical_crossentropy
 import numpy
 
 def softmax(x):
+    """Calucate the softmax of a vector"""
     e_x = numpy.exp(x - numpy.max(x))
     out = e_x / e_x.sum()
     return out
 
 def possible_move_indexes(board):
-    if board.shape == (3,3):
-        board_ = board.reshape((9,))
-    else:
-        board_ = board
-
-    idx = numpy.argwhere(board_)
-    return idx.reshape(len(idx)).tolist()
+    board_ = board.reshape(9)
+    return numpy.where(board_==0)[0].tolist()
     
 def alphazero_loss(y_true, y_pred):
+    #TODO: is it actually categorical_crossentropy?
     policy_vector_true = y_true[0]
     policy_vector_pred = y_pred[0]
     evaluation_true = y_true[1]
@@ -28,7 +25,8 @@ def alphazero_loss(y_true, y_pred):
     return sum((mean_squared_error(evaluation_true, evaluation_pred),
             categorical_crossentropy(policy_vector_true, policy_vector_pred)))
 
-def createModel():
+def createModel(config=None):
+    #TODO: add config to make changing hyper params easier
     board_input = Input(shape=(1,3,3), name="main_input", dtype = 'float32')
 
     conv1 = Conv2D(1,
@@ -90,36 +88,52 @@ def createModel():
     flat2 = Flatten()(activation)
 
     policy_vector = Dense(9, activation="relu")(flat1)
+    softmax = Activation('softmax')(policy_vector)
     e = Dense(1)(flat2)
-    model = Model(inputs=[board_input], outputs=[policy_vector, e])
+
+    model = Model(inputs=[board_input], outputs=[softmax, e, policy_vector])
     model.compile(optimizer='sgd',
-            loss = alphazero_loss)
+            loss = alphazero_loss,
+            metrics=['accuracy'])
     return model
 
 class NN:
+    def __init__(self, existing=None, config=None):
+        if existing:
+            self.nn = keras.models.load_model(existing)
+        else:
+            self.nn = createModel(config=config)
+
     def valid_policy(self, policy, board):
         board_ = board.reshape(9)
-        p_moves = possible_move_indexes(board)
+        p_moves = possible_move_indexes(board_)
         new_policy = []
         for idx in p_moves:
-            new_policy.append(board[idx])
+            new_policy.append(policy[idx])
         new_policy = numpy.array(new_policy)
-        new_policy.dtype = "float32"
+        new_policy = new_policy.astype("float32")
         return softmax(new_policy)
 
+    def predict_policy(self, board):
+        board_ = board.reshape((1,1,3,3))
+        _, __, policy = self.nn.predict(board_)
+        policy = policy[0].tolist()
+        return self.valid_policy(policy, board_)
+        raise RuntimeError("Make sure you have a layer called before-softmax")
 
-    def __init__(self):
-        self.nn = createModel()
+    def predict_score(self, board):
+        board_ = board.reshape((1,1,3,3))
+        _, score, __ = self.nn.predict(board_)
+        return score[0][0]
 
+    def train(self, inputs, policies, scores):
+        """
+        Inputs are a numpy array with the shape (x, 1, 3, 3)
+        Policies are a numpy array with the shape (x, 9)
+        Scores are a numpy array with the shape (x)
+        """
+        self.nn.fit(inputs, [policies, scores, policies])
 
-#policy = model.predict(numpy.array([0,0,0,0,0,0,0,0,0]).reshape((1,1,3,3)))[0]
-#print(policy)
-#print(valid_policy(policy, numpy.array([1,1,1,0,0,0,0,0,0])))
-
-
-
-
-
-
-
+    def save(self, filename):
+        self.nn.save(filename)
 
